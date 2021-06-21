01332900ec614e0cbb0a29801a17901aa20a8477
VCacciatore <vincent@DESKTOP-JJAUDHT.localdomain>
2021-06-21 13:00:56 -0500

    expanded site urls

--- web_scrapers/teaScraper.py
+++ web_scrapers/teaScraper.py
@@ -2,29 +2,35 @@ import requests
 import pandas as pd
 import json
 
-url = 'https://www.harney.com/collections/black-tea/products.json'
-
-r = requests.get(url)
-
-data = r.json()
+urls = ['https://www.harney.com/collections/black-tea/products.json',
+        'https://ahmadteausa.com/collections/popular/products.json',
+        'https://tea-tao.com/collections/loose-tea-in-pouch/products.json']
 
 tea_list = []
 
-for item in data['products']:
-    name = (item['title'])
-    for variant in item['variants']:
-        price = variant['price']
-        size = variant['grams']
-    
-        tea = {
-            'name': name,
-            'price': price,
-            'size in grams': size,
-        }
-    
-        tea_list.append(tea)
+for url in urls:
 
-#print(tea_list)
+    r = requests.get(url)
+
+    data = r.json()
+
+    
+
+    for item in data['products']:
+        name = (item['title'])
+        for variant in item['variants']:
+            price = variant['price']
+            size = variant['grams']
+        
+            tea = {
+                'name': name,
+                'price': price,
+                'size in grams': size,
+            }
+        
+            tea_list.append(tea)
+
+    #print(tea_list)
 
 df = pd.DataFrame(tea_list)
 
